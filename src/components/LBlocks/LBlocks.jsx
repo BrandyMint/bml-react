4fc57e898abf01fdb2a455bb12809ddcb41c09fa
Danil Pismenny <danil@brandymint.ru>
2016-01-30 13:24:34 +0300

    animation timeout in constants

--- src/components/LBlocks/LBlocks.jsx
+++ src/components/LBlocks/LBlocks.jsx
@@ -12,6 +12,8 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import LBlock from 'components/LBlock';
 import LBlockAddButton from 'components/LBlockAddButton';
 
+const TRANSITION_TIMEOUT = 300;
+
 const Placeholder = () => (
   <div className="LBlocks-placeholder">
     <span>There is no blocks to display</span>
@@ -37,8 +39,8 @@ const LBlocks = ({ blocks, isEditMode, onAddBlock }) => {
           <ReactCSSTransitionGroup
             component="div"
             transitionName="animation"
-            transitionEnterTimeout={300}
-            transitionLeaveTimeout={300}
+            transitionEnterTimeout={TRANSITION_TIMEOUT}
+            transitionLeaveTimeout={TRANSITION_TIMEOUT}
           >
             {map(blocks, renderSection)}
           </ReactCSSTransitionGroup>
