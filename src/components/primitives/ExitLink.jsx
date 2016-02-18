57ce26ffc9f005f1b0a578d3b2092ad303452e94
Danil Pismenny <danil@brandymint.ru>
2016-02-18 22:15:35 +0300

    minor

--- src/components/primitives/ExitLink.jsx
+++ src/components/primitives/ExitLink.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { createSelector } from 'reselect';
 
-import ExitIcon from 'react-icons/lib/md/exit-to-app';
+import ExitIcon from 'react-icons/lib/md/exit-to_app';
 
 const ExitLink = ({ exitUrl }) => (
   <Link to={exitUrl} className="IconLink">
