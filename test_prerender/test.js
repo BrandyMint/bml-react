54ab925faae6207e5497de0e6d7ba51230693969
Danil Pismenny <danil@brandymint.ru>
2016-02-26 09:33:15 +0300

    test prerender

--- test_prerender/test.js
+++ test_prerender/test.js
@@ -1,10 +1,10 @@
 var global = global || this;
 var self = self || this;
 var window = window || this;
-require('./test/react-server.js');
+require('./test_prerender/react-server.js');
 require('./dist/viewer-prerender.js');
 
-var blocks = require('./test/blocks.js');
+var blocks = require('./test_prerender/blocks.js');
 
 console.log(
   ReactDOMServer.renderToString(React.createElement(ShowWrapper, { blocks: blocks }))
