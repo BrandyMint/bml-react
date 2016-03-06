ab77306d28c7d0d921b70a8a27e8c48a7558c1d6
Danil Pismenny <danil@brandymint.ru>
2016-03-06 21:34:20 +0300

    minor

--- src/components/NoMatch/index.js
+++ src/components/NoMatch/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router';
+import map from 'lodash/map';
 
 import { EXAMPLE_LANDING_VARIANT_UUID } from 'constants/initialState';
 
+const LANDINGS = [
+  {
+    uuid: EXAMPLE_LANDING_VARIANT_UUID,
+    title: 'Пример лендинга из API',
+  },
+  {
+    uuid: 'fullExamples',
+    title: 'Примеры всех блоков во всех вариантах',
+  },
+];
+
 const NoMatch = () => (
-  <div>
+  <div className="container">
     <h2>Такая страница не найдена</h2>
-    <Link to={`/editor/${EXAMPLE_LANDING_VARIANT_UUID}`}>Пример лендинга</Link>
+    <ul>
+      {map(LANDINGS, (l, index) =>
+        <li key={index}>
+          <Link to={`/editor/${l.uuid}`}>{l.title}</Link>
+        </li>
+      )}
+     </ul>
   </div>
 );
 
