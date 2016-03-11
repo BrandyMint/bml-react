885cbf9b45d1e6c1b3c4217272a13c6214ea7010
Danil Pismenny <danil@orionet.ru>
2016-03-11 17:03:04 +0300

    update config

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,9 +3,11 @@ import { EXAMPLE_LANDING_VARIANT_UUID } from 'constants/initialState';
 
 const defaults = {
   exitUrl: '/#exit',
-  apiUrl: 'http://api.3008.vkontraste.ru/v1',
-  postLeadUrl: 'http://3008.vkontraste.ru/leads',
-  apiKey: '5d8aa2f240c5d05e992e0e84f58ce965',
+  apiUrl: 'http://api.bmland.dev:3008/v1',
+  postLeadUrl: 'http://bmland.dev:3008/leads',
+  //apiUrl: 'http://api.3008.vkontraste.ru/v1',
+  //postLeadUrl: 'http://3008.vkontraste.ru/leads',
+  apiKey: 'demoApiKey',
   variantUuid: EXAMPLE_LANDING_VARIANT_UUID,
 };
 
