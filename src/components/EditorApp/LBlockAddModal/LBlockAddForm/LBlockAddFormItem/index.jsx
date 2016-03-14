80451f74c0b136c8a56d991c9e17f2021817bad8
Danil Pismenny <danil@brandymint.ru>
2016-03-14 22:07:41 +0300

    Подправил форму добавления блоков

--- src/components/EditorApp/LBlockAddModal/LBlockAddForm/LBlockAddFormItem/index.jsx
+++ src/components/EditorApp/LBlockAddModal/LBlockAddForm/LBlockAddFormItem/index.jsx
@@ -15,23 +15,7 @@ const LBlockAddFormItem = ({
     className={classnames('LBlockAddFormItem', { 'is-selected': isSelected })}
     onClick={onSelect}
   >
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="LBlockAddFormItem-image">
-            <img src={image.url} height={100} />
-          </div>
-        </div>
-        <div className="col-sm-8">
-          <div className="LBlockAddFormItem-content">
-            <h5 className="LBlockAddFormItem-title">{title}</h5>
-            <p className="LBlockAddFormItem-description">
-              {description}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+  <h5 className="LBlockAddFormItem-title">{title}</h5>
   </div>
 );
 
