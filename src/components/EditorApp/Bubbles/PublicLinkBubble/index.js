6bafa42ce0ee57d978c05f97cc80329cdfe10e51
Danil Pismenny <danil@brandymint.ru>
2016-03-30 15:58:59 +0300

    Ссылка на конкретный вариант из публичной части

--- src/components/EditorApp/Bubbles/PublicLinkBubble/index.js
+++ src/components/EditorApp/Bubbles/PublicLinkBubble/index.js
@@ -1,6 +1,15 @@
 import { connect } from 'react-redux';
 import component from './component';
+import urlParse from 'url-parse';
 
-const selector = ({ site }) => ({ public_url: site.public_url });
+const selector = ({ site: { public_url }, application: { variantUuid } }) => {
+  const url = urlParse(public_url);
+
+  url.query = `variant_uuid=${variantUuid}`;
+
+  return {
+    public_url: url.toString(),
+  };
+};
 
 export default connect(selector)(component);
