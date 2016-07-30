d1986c76194685205a63cf02404c7b758308742e
Danil Pismenny <danil@brandymint.ru>
2016-07-30 13:47:49 +0300

    add favicon

--- ghpages.sh
+++ ghpages.sh
@@ -1,3 +1,3 @@
 DIST=./tmp/ghpages
-rm -fr $DIST && mkdir $DIST && cp -vr ./src/assets ./dist-editor/* $DIST && cd $DIST && rm stats.json && \
+rm -fr $DIST && mkdir $DIST && cp -vr ./src/assets ./dist-editor/* $DIST && cp -vr ./public/* $DIST && cd $DIST && rm stats.json && echo yopage.ru > CNAME && \
 git init && git remote add origin git@github.com:BrandyMint/bml-react.git && git add . && git commit -m 'publicate ghpages' && git checkout -b ghpages && git push -f origin ghpages
